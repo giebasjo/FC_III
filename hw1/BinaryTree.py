b265e2cd659294993dd277648cf933a943a37536
Jordan Giebas <jgiebas@andrew.cmu.edu>
2018-03-27 15:16:13 -0400

    Fixed the BinaryTree class depth method

--- hw1/BinaryTree.py
+++ hw1/BinaryTree.py
@@ -3,34 +3,96 @@
 # Author(s): Jordan Giebas
 
 class BinaryTree:
+
     class _BTNode:
         def __init__(self, value, left = None, right = None):
             self._value = value
             self._left = left
             self._right = right
+
     def __init__(self):
-        self._top = None
+        self._top  = None
+        self._list = list()
+
     def insert(self, value):
+
         if self._top == None:
             self._top = self._BTNode(value)
+            self._list.append( str(value) )
         else:
-            ... yours to finish ...
+            if value < self._top._value:
+                if self._top._left == None:
+                    self._top._left = self._BTNode(value)
+                    self._list.append( str(value) )
+                else:
+                    self._top = self._top._left
+                    self.insert(value)
+            elif value > self._top._value:
+                if self._top._right == None:
+                    self._top._right == self._BTNode(value)
+                    self._list.append( str(value) )
+                else:
+                    self._top = self._top._right
+                    self.insert(value)
 
+    def __str__(self):
+        return " ".join(self._list)
+
+    def size(self):
+        return len(self._list)
+
+    def depth_helper(self, node):
+
+        if node is None:
+            return 0
+        else:
+            l_depth = self.depth_helper(node._left)
+            r_depth = self.depth_helper(node._right)
+
+        return max(l_depth, r_depth) + 1
+
+    def depth(self):
+        return self.depth_helper(self._top)
+
+
+    def print_pretty_helper(self, node, level):
+
+        if self._top == None:
+            return ""
+        
+        self.print_pretty_helper(self._top._right, level + 1)
+        for i in range(level):
+            print('\t')
+        
+        print(self._top._value)
+        self.print_pretty_helper(self._top._left, level + 1)
+
+    def print_pretty(self):
+
+        if self._top == None:
+            print("True")
+            return ""
+
+        self.print_pretty_helper(self._top,0)
 
 
 # test code:
-
 bt = BinaryTree()
-print(bt)
-print(bt.size())
-print(bt.depth())
+print("Printing Binary Tree: ", bt)
+print("Size: ", bt.size())
+print("Depth: ", bt.depth())
+print("--inserting--")
 bt.insert(7)
 bt.insert(3)
 bt.insert(2)
 bt.insert(13)
 bt.insert(9)
 bt.insert(3)
-print(bt)
-print(bt.size())
-print(bt.depth())
-bt.print_pretty()
+print("--done inserting--")
+print("Printing Binary Tree: ", bt)
+print("Tree Size: ", bt.size())
+print("Tree Depth: ", bt.depth())
+#print("Printing pretty --- ")
+#bt.print_pretty()
+
+
